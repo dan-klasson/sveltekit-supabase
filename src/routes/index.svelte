bc2a40de059ecb16a121ec8905f1599b2ce101fb
Daniel Klasson <dan.g.klasson@gmail.com>
2022-01-30 13:33:58 +0100

    Install tailwindcss

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="container mx-auto">
+  <h1 class="text-2xl font-bold ">
+    Install Tailwind CSS 3
+    <span class="text-transparent bg-clip-text bg-gradient-to-r from-yellow-400 via-orange-500 to-purple-600">
+      In SvelteKit
+    </span>
+  </h1>
+</div>
